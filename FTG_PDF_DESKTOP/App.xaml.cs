110f8be7879a0ab42fbbc2be2298de09b387cadd
bryce standley <bryce@vectorpixel.net>
2025-06-15 15:23:10 +0800

    Cleaned up project and added desktop app

---
+++ FTG_PDF_DESKTOP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace FTG_PDF_DESKTOP;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ FTG_PDF_DESKTOP/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FTG_PDF_DESKTOP;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
